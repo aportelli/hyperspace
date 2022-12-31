7a4c297e8b91dd1770067a475adfce84e4037628
Antonin Portelli <antonin.portelli@me.com>
2022-12-31 18:02:58 +0000

    Add hash functions with tests

---
+++ index/hash.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2022 Antonin Portelli <antonin.portelli@me.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package index
+
+import (
+	"crypto/md5"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+)
+
+// Return the hash of a path as int64 (SQLite does not support uint64).
+// The hash is a 48 bit hash defined as follows
+//
+//  1. compute the MD5 hash of the path
+//  2. take the 8 highest bytes of the MD5 hash
+//  3. set the 2 highest bytes to zero
+//  4. convert the result to int64 in big endian
+func PathHash(path string) int64 {
+	fullHash := md5.Sum([]byte(path))
+	copy(fullHash[:2], "\x00\x00")
+	hash := int64(binary.BigEndian.Uint64(fullHash[:8]))
+	return hash
+}
+
+// Convert a path hash to an hexadecimal string
+func HashToString(hash int64) string {
+	byteHash := make([]byte, 8)
+	binary.BigEndian.PutUint64(byteHash, (uint64)(hash))
+	byteHash = byteHash[2:]
+	return fmt.Sprintf("%x", byteHash)
+}
+
+// Convert a path hash hexadecimal string to int64
+func StringToHash(hash string) (int64, error) {
+	byteHash, err := hex.DecodeString(hash)
+	if err != nil {
+		return 0, err
+	}
+	byteHash = append([]byte{0, 0}, byteHash...)
+	intHash := int64(binary.BigEndian.Uint64(byteHash))
+	return intHash, nil
+}
---
+++ index/tests/hash_test.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2022 Antonin Portelli <antonin.portelli@me.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package index
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/aportelli/hyperspace/index"
+)
+
+type hashTest struct {
+	path       string
+	intHash    int64
+	stringHash string
+}
+
+var hashTests = []hashTest{
+	{"/dir1/dir2/dir3/dir4", 126142529671152, "72b9d8ac0ff0"},
+	{"/usr/bin/bash", 161059760740939, "927ba5d39a4b"},
+	{"any string", 47700301343738, "2B6217846BFA"},
+}
+
+func TestHash(t *testing.T) {
+	for _, test := range hashTests {
+		intHash := index.PathHash(test.path)
+		if intHash != test.intHash {
+			t.Errorf("Got int64 hash %x, expected %x\n", intHash, test.intHash)
+		}
+	}
+}
+
+func TestStringToHash(t *testing.T) {
+	for _, test := range hashTests {
+		stringHash := index.HashToString(test.intHash)
+		if !strings.EqualFold(stringHash, test.stringHash) {
+			t.Errorf("Got string hash %s, expected %s\n", stringHash, test.stringHash)
+		}
+	}
+}
+
+func TestHashToString(t *testing.T) {
+	for _, test := range hashTests {
+		intHash, err := index.StringToHash(test.stringHash)
+		if err != nil {
+			t.Errorf("Got error %s", err.Error())
+		}
+		if intHash != test.intHash {
+			t.Errorf("Got hash %x, expected %x\n", intHash, test.intHash)
+		}
+	}
+}
